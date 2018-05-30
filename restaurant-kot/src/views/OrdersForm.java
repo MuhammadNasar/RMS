d259c6075b7676bed057984ad98c53beda5a879a
Muhammad Nasar <muhammadnasar008@gmail.com>
2018-05-30 10:49:29 +0500

    minor changes
    minor changes

--- restaurant-kot/src/views/OrdersForm.java
+++ restaurant-kot/src/views/OrdersForm.java
@@ -14,9 +14,6 @@ import javax.swing.BorderFactory;
  */
 public class OrdersForm extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form OrdersForm
-     */
     public OrdersForm() {
         initComponents();
         
@@ -44,27 +41,15 @@ public class OrdersForm extends javax.swing.JInternalFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-<<<<<<< HEAD
                 .addGap(294, 294, 294)
                 .addComponent(jLabel1)
                 .addContainerGap(295, Short.MAX_VALUE))
-=======
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(329, Short.MAX_VALUE))
->>>>>>> master
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-<<<<<<< HEAD
                 .addComponent(jLabel1)
                 .addGap(0, 358, Short.MAX_VALUE))
-=======
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(257, Short.MAX_VALUE))
->>>>>>> master
         );
 
         pack();
