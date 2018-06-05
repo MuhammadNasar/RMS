b66c29aa8ad769135410138f805f52806bd76270
sohail khan <sohail.uap@gmail.com>
2018-06-05 12:01:22 +0500

    waiters Form completed
    waiters Form completed

--- restaurant-kot/src/services/WaitersServices.java
+++ restaurant-kot/src/services/WaitersServices.java
@@ -9,67 +9,52 @@ import javax.swing.JOptionPane;
 import util.SQLQueryUtil;
 
 public class WaitersServices {
-    
-   private WaitersDAO waitersDAO;
-   private Component rootPane;
+
+    private WaitersDAO waitersDAO;
+    private Component rootPane;
 
     public WaitersServices() {
         waitersDAO = new WaitersDAO();
     }
-    
-    public void waiter(Waiters waiters){
-        
-    SQLQueryUtil sql= new SQLQueryUtil();
-    sql.connect(false);
-    String query,queryCheck;
-    
-    try {
-                queryCheck = "select count(*) as count from waiters where"
-                        + " waiter_number ='" + waiters.getWaiterNumber()+ "'"
-                        + " OR cnic ='" + waiters.getCnic()+ "';";
-                       
-    System.out.println("i am here");
-    ResultSet rs = sql.executeQuery(queryCheck);
-                rs.next();
-                int data = rs.getInt("count");
+
+    public void waiter(Waiters waiters) {
+
+        SQLQueryUtil sql = new SQLQueryUtil();
+        sql.connect(false);
+        String query, queryCheck;
+        String queryString;
+        int canInserted = 0;
+
+        try {
+            queryCheck = "select count(*) as count from waiters where"
+                    + " waiter_number ='" + waiters.getWaiterNumber() + "'"
+                    + " OR cnic ='" + waiters.getCnic() + "';";
+
+            System.out.println("i am here");
+            
+            ResultSet rs = sql.executeQuery(queryCheck);
+            
+            rs.next();
+            int data = rs.getInt("count");
+            
+
+            if (data == 0) {
                 
-                    
-                    System.out.println(query);
-                       if (data == 0) {
-                    System.out.println("inside if");
-                    int rowAffected = sql.executeUpdate(query);
-                    if (rowAffected == 1) {
-                                            System.out.println("before commit");
-                        sql.commit();
-                                            System.out.println("after commit");
-                        JOptionPane.showMessageDialog(rootPane, "waiters successfully registered");
-                    }
-
-                } else {
-                    JOptionPane.showMessageDialog(rootPane, "Already exists.");
-
-                }
-
-            } catch (SQLException sq) {
-                sq.printStackTrace();
-
-            } finally {
-                sql.disconnect();
+                
+                System.out.println("inside if");
+                
+                waitersDAO.registerWaiters(waiters);
+                JOptionPane.showMessageDialog(rootPane," Waiter register successfully");
+                
+            } else {
+                JOptionPane.showMessageDialog(rootPane, "Already exists.");
             }
+
+        } catch (SQLException sq) {
+            sq.printStackTrace();
+
+        } finally {
+            sql.disconnect();
+        }
     }
 }
-    
-    
-   
-       
-    
- 
-    
-    
-    
-        
-   
-            
-          
-                
-     
